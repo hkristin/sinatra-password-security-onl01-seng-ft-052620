3b0e78725c6209146c489dd83f37bb7177dbdf18
Kristin Howard <hkristin@andrew.cmu.edu>
2020-07-16 02:57:08 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,8 @@ class ApplicationController < Sinatra::Base
 
 	post "/login" do
 		user = User.find_by(:username => params[:username])
-		if user
+		if user && user.authenticate(params[:password])
+		  session[:user_id]
 		  redirect to "/success"
 		else 
 		  redirect to "/failure"
