82c43962e6668a3bb57f1cdd4f4300aab1e5103b
Kristin Howard <hkristin@andrew.cmu.edu>
2020-07-16 02:51:36 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,11 @@ class ApplicationController < Sinatra::Base
 
 	post "/signup" do
 		user = User.new(:username => params[:username], params[:password])
+		if user.save
+		  redirect to "/login"
+		else 
+		  redirect to "/failure"
+		end
 	end
 
 	get "/login" do
